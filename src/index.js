79188a5e0c71091fc36da975454c927d9969158c
natolij8585 <anatolij8585@bk.ru>
2022-11-29 09:15:48 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function toReadable (number) {
-  
+    let zero = 'zero';
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let hundreds = ' hundred';
+    let string = number.toString();
+    if (number === 0) {
+    return zero;
+    }
+    if (number < 20) {
+         return (ones[number]).trim();
+    }
+    if (string.length === 2) {
+        return (tens[string[0]] + ' ' + ones[string[1]]).trim();
+    }
+    if (string.length == 3) {
+        if (string[1] == '0' && string[2] == '0') {
+            return (ones[string[0]] + hundreds).trim();
+        } else {
+        return (ones[string[0]] + hundreds + ' ' + toReadable(+(string[1] + string[2]))).trim();
+        }
+    }
 }
